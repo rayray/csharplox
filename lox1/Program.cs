d4c60f25f14a9f29d8f18c7dc16c2c6ea05dec78
Raymond Edwards <raymond.edwards@gmail.com>
2021-08-07 02:35:29 -0400

    Read and print text

---
+++ lox1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace lox1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length > 1)
+			{
+				Console.WriteLine("Usage: csharplox [script]");
+				return;
+			} 
+			else if (args.Length == 1)
+			{
+				RunFile(args[0]);
+			}
+			else
+			{
+				RunPrompt();
+			}
+		}
+
+		private static void RunFile(string path)
+		{
+			if (!File.Exists(path))
+			{
+				Console.WriteLine("file does not exist");
+			}
+
+			string source = File.ReadAllText(path);
+			Run(source);
+		}
+
+		private static void RunPrompt()
+		{
+			while (true)
+			{
+				Console.Write("> ");
+				string line = Console.ReadLine();
+				if (line == null) break;
+				Run(line);
+			}
+		}
+
+		private static void Run(string source)
+		{
+			Console.WriteLine(source);
+		}
+	}
+}
